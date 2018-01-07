c2d66795cc4d4cbfb474a686662d6963cd02b7b2
thaonguyen96 <34390525+thaonguyen96@users.noreply.github.com>
2018-01-08 01:38:37 +0700

    Add files via upload

---
+++ frmQuanLyDatVe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyChuyenBay
+{
+    public partial class frmQuanLyDatVe : Form
+    {
+        public frmQuanLyDatVe()
+        {
+            InitializeComponent();
+        }
+    }
+}
