a470754b0173cc6722de3991b85a542f3f3a7293
thaonguyen96 <34390525+thaonguyen96@users.noreply.github.com>
2018-01-08 11:27:34 +0700

    Add files via upload

--- frmLogin.cs
+++ frmLogin.cs
@@ -14,4 +14,26 @@ namespace QuanLyChuyenBay
     {
         this.Close();
     }
+    private void btnDangnhap_click(object sender, EventArgs e)
+    {
+        SqlConnection conn = new SqlConnection(chuoikn);
+        string sqlSelect = "Select * from tblTaikhoan where Taikhoan=" + txtTaikhoan.Text + "and Matkhaau=" + txtMatkhau.Text+"
+        conn.Open();
+        SQLCommand cmd = new SQLCommand(sqlSelect, conn);
+        SqlDataReader reader = cmd.ExecuteReader();
+        if(reader.Read()==true)
+        {
+            this.Hide();
+            frmMain = new frmMain();
+            Main.Show();
+
+        }
+        else
+        {
+            MessageBox.Show("ban dang nhap không thành công");
+            txtTaikhoan.Text = "";
+            txtMatkhau.text = "";
+            txtTaikhoan.Focus();
+        }
+    }
 }
